776ecef524077f375166c23b8fed2c54f95a95b4
vitin api <ra54573958800@acaonsfatima.org.br>
2024-08-02 18:11:50 -0300

    jmdos

---
+++ soma.js
@@ -0,0 +1,14 @@
+import express from 'express'
+const servidor = express()
+
+servidor.listen(100,() => console.log(`API SUBIU`))
+
+servidor.get('/calculadora/:n1/:n2', (req,resp) => {
+    let n1 = Number(req.params.n1);
+    let n2 = Number(req.params.n2);
+
+    let soma = n1 + n2
+
+   resp.send(`A soma desses dois numéros é ` + soma)
+})
+
